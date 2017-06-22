442f679b2daedd28ea393362e6cafa1b8e7358ee
DaVinci42 <starrynight.ying@gmail.com>
2017-06-22 12:06:43 +0800

    add #26

---
+++ Python/26.Remove Duplicates from Sorted Array.py
@@ -0,0 +1,34 @@
+"""
+26. Remove Duplicates from Sorted Array
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+"""
+
+
+class Solution(object):
+
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return 1
+
+        pre, length = nums[0] - 1, 0
+        for i in range(0, len(nums)):
+            v = nums[i]
+            if v != pre:
+                nums[length] = v
+                length += 1
+                pre = v
+        return length
