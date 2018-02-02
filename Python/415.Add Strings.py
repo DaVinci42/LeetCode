52a8a2632626f294da45a8a29c20beb8c8403c5a
Dan Xin <starrynight.ying@gmail.com>
2018-02-02 17:10:14 +0800

    add #415

--- Python/415.Add Strings.py
+++ Python/415.Add Strings.py
@@ -1,43 +1,70 @@
 """
-415. Add Strings
+413. Arithmetic Slices
 
-Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+A sequence of number is called arithmetic if it consists of at least three elements
+and if the difference between any two consecutive elements is the same.
 
-Note:
+For example, these are arithmetic sequence:
 
-The length of both num1 and num2 is < 5100.
-Both num1 and num2 contains only digits 0-9.
-Both num1 and num2 does not contain any leading zero.
-You must not use any built-in BigInteger library or convert the inputs to integer directly.
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic.
+
+1, 1, 2, 5, 7
+
+A zero-indexed array A consisting of N numbers is given.
+A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+A slice (P, Q) of array A is called arithmetic if the sequence:
+A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+The function should return the number of arithmetic slices in the array A.
+
+
+Example:
+
+A = [1, 2, 3, 4]
+
+return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
 """
 
 
 class Solution:
 
-    def addStrings(self, num1, num2):
+    def numberOfArithmeticSlices(self, A):
         """
-        :type num1: str
-        :type num2: str
-        :rtype: str
+        :type A: List[int]
+        :rtype: int
         """
-        if num1 is None or len(num1) == 0:
-            return num2
-        if num2 is None or len(num2) == 0:
-            return num1
+        if A is None or len(A) < 3:
+            return 0
+        diff_list = list()
+        for i in range(1, len(A)):
+            diff = A[i] - A[i - 1]
+            diff_list.append(diff)
+        result = 0
+        pre_diff, repeat_count = diff_list[0], 0
+        for d in diff_list:
+            if d == pre_diff:
+                repeat_count += 1
+            else:
+                if repeat_count >= 3:
+                    result += self.combine_in_count(repeat_count)
+                pre_diff = d
+                repeat_count = 1
+        if repeat_count >= 3:
+            result += self.combine_in_count(repeat_count)
+        return result
 
-        idx1, idx2 = len(num1) - 1, len(num2) - 1
-        adding = False
-        result = list()
-        while idx1 >= 0 or idx2 >= 0 or adding:
-            v1, v2 = 0, 0
-            if idx1 >= 0:
-                v1 = int(num1[idx1])
-                idx1 -= 1
-            if idx2 >= 0:
-                v2 = int(num2[idx2])
-                idx2 -= 1
-            sum = v1 + v2 if not adding else v1 + v2 + 1
-            adding = sum >= 10
-            sum %= 10
-            result.append(sum)
-        return "".join(map(lambda x: str(x), result[::-1]))
+    def combine_in_count(self, count):
+        if count < 3:
+            return 0
+        elif count == 3:
+            return 1
+        else:
+            return (1 + count - 2) / 2 * (count - 2)
+
+
+s = Solution()
+print(s.numberOfArithmeticSlices([1, 3, 5, 7, 9]))
