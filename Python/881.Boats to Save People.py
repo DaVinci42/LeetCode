1f3e32c32fa52a9a632d021913933705542dbab7
Dan Xin <davinci42.cn@gmail.com>
2018-11-20 14:44:40 +0800

    add #881

---
+++ Python/881.Boats to Save People.py
@@ -0,0 +1,57 @@
+"""
+881. Boats to Save People
+
+The i-th person has weight people[i], and each boat can carry a maximum weight of limit.
+
+Each boat carries at most 2 people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person. (It is guaranteed each person can be carried by a boat.)
+
+Example 1:
+
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+Example 2:
+
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+Example 3:
+
+Input: people = [3,5,3,4], limit = 5
+Output: 4
+Explanation: 4 boats (3), (3), (4), (5)
+Note:
+
+1 <= people.length <= 50000
+1 <= people[i] <= limit <= 30000
+"""
+
+
+class Solution:
+
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        if not people:
+            return 0
+
+        people.sort()
+        used_boat = 0
+        left, right = 0, len(people) - 1
+        while right > left:
+            weight_left, weight_right = people[left], people[right]
+            if weight_left + weight_right > limit:
+                right -= 1
+                used_boat += 1
+            else:
+                right -= 1
+                left += 1
+                used_boat += 1
+        if right == left:
+            used_boat += 1
+        return used_boat
