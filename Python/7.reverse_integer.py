f3dc0ea4b9b259707e0bed179f4f81a916211e9a
Dan Xin <davinci42.cn@gmail.com>
2020-10-13 23:50:33 +0800

    #7

---
+++ Python/7.reverse_integer.py
@@ -0,0 +1,11 @@
+import math
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = str(x)[0] == '-'
+        result = -1 * int(str(x)[1:][::-1]) if negative else int(str(x)[::-1])
+        if -1 * math.pow(2, 31) <= result <= math.pow(2, 31) - 1:
+            return result
+        else:
+            return 0
