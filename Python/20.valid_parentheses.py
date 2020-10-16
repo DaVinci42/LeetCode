38a61e1ca829d11f81492deabb98c3f13354dce3
Dan Xin <davinci42.cn@gmail.com>
2020-10-16 14:28:45 +0800

    #20

---
+++ Python/20.valid_parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+
+        map = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        stack = []
+        for c in s:
+            if c not in map:
+                if not stack or map[stack.pop()] != c:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
