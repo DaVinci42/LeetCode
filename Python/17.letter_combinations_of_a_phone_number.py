e28b455e12af53df40f6d35fa5594165cd0bc622
Dan Xin <davinci42.cn@gmail.com>
2020-10-20 23:21:32 +0800

    #17

---
+++ Python/17.letter_combinations_of_a_phone_number.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        digit_map = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        result = []
+        for d in digits:
+            result = self.combine(result, digit_map[d])
+        return result
+
+    def combine(self, left: List[str], right: List[str]) -> List[str]:
+        if not left:
+            return right
+        return [a + b for a in left for b in right]
