8f42d7ef7bafaef8178a11f45bae836c94b7b286
Dan Xin <davinci42.cn@gmail.com>
2020-10-22 23:04:44 +0800

    optimize #106

--- 106.ConstructBinaryTreefromInorderandPostorderTraversal.py
+++ 106.ConstructBinaryTreefromInorderandPostorderTraversal.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 
 # Definition for a binary tree node.
@@ -13,9 +13,52 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         if not inorder or not postorder:
             return None
-        indexRoot = inorder.index(postorder[-1])
+
+        return self.build(
+            inorder,
+            0,
+            len(inorder) - 1,
+            postorder,
+            0,
+            len(postorder) - 1,
+            {k: i for i, k in enumerate(inorder)},
+        )
+
+    def build(
+        self,
+        inorder: List[int],
+        inLeft: int,
+        inRight: int,
+        postorder: List[int],
+        postLeft: int,
+        postRight: int,
+        indexCache: Dict[int, int],
+    ) -> TreeNode:
+        if postLeft > postRight:
+            return None
+        elif postLeft == postRight:
+            return TreeNode(postorder[postRight])
+
+        root = postorder[postRight]
+        index = indexCache[root]
         return TreeNode(
-            postorder[-1],
-            self.buildTree(inorder[:indexRoot], postorder[:indexRoot]),
-            self.buildTree(inorder[indexRoot + 1 :], postorder[indexRoot:-1]),
+            root,
+            self.build(
+                inorder,
+                inLeft,
+                max(0, index - 1),
+                postorder,
+                postLeft,
+                postLeft + index - inLeft - 1,
+                indexCache,
+            ),
+            self.build(
+                inorder,
+                index + 1,
+                inRight,
+                postorder,
+                postLeft + index - inLeft,
+                postRight - 1,
+                indexCache,
+            ),
         )
