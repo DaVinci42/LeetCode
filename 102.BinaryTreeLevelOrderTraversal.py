9339497333b2132abba30dbec31376e5a9a12739
Dan Xin <davinci42.cn@gmail.com>
2020-10-22 17:49:42 +0800

    #102

---
+++ 102.BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        l, result = [root], []
+        while l:
+            layer, nextL = [], []
+            for n in l:
+                layer.append(n.val)
+                if n.left:
+                    nextL.append(n.left)
+                if n.right:
+                    nextL.append(n.right)
+            result.append(layer)
+            l = nextL
+        return result
