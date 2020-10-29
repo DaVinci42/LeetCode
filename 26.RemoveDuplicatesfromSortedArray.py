8347d88c41f2539550593cad339f000ecb82af28
Dan Xin <davinci42.cn@gmail.com>
2020-10-29 14:55:04 +0800

    #26

---
+++ 26.RemoveDuplicatesfromSortedArray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        i, j = 0, 0
+        while j < len(nums):
+            if nums[j] == nums[i]:
+                j += 1
+            else:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
