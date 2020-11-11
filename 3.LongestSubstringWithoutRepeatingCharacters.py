f6d382cbd40c34b8d12408e430293ec627388b6b
Dan Xin <davinci42.cn@gmail.com>
2020-11-11 18:33:35 +0800

    optimize #3

--- 3.LongestSubstringWithoutRepeatingCharacters.py
+++ 3.LongestSubstringWithoutRepeatingCharacters.py
@@ -1,8 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ss = ""
-        for c in s:
-            ss += c
-            if len(ss) != len(set(ss)):
-                ss = ss[1:]
-        return len(ss)
+        if not s:
+            return 0
+
+        cache = {}
+
+        def grow(start: int, cur: int) -> int:
+            for i in range(cur, len(s)):
+                c = s[i]
+                if c not in cache or cache[c] < start:
+                    cache[c] = i
+                else:
+                    curLen = i - start
+                    preC = cache[c]
+                    cache[c] = i
+                    return max(curLen, grow(preC + 1, i + 1))
+            return len(s) - start
+
+        return grow(0, 0)
